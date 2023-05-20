a24b420b5add0cbf29e25365a01cd73bd6c7fad3
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-05-20 14:46:31 +0300

    Add new function detail view of comments

--- RestFul Routes/app.js
+++ RestFul Routes/app.js
@@ -10,22 +10,27 @@ app.use(express.json());
 
 let comments = [
   {
+    id: 1,
     username: "monty",
     comment: "aha aha aha good.",
   },
   {
+    id: 2,
     username: "aury",
     comment: "i have seen it.",
   },
   {
+    id: 3,
     username: "notty",
     comment: "can you do it again.",
   },
   {
+    id: 4,
     username: "jhinku",
     comment: "not all that good.",
   },
   {
+    id: 5,
     username: "shukku",
     comment: "tere bin ghorib.",
   },
@@ -37,6 +42,12 @@ app.get("/comments", (req, res) => {
 
 app.get("/comments/new", (req, res) => {
   res.render("comments/new");
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  let comment = comments.find((c) => c.id === parseInt(id));
+  res.render("comments/details", { comment });
 });
 
 app.post("/comments", (req, res) => {
