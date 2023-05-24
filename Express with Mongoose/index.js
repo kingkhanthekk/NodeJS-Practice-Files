d98898510b540d19280252ef6d137eace2a5446a
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-05-24 15:56:21 +0300

    Update product function added and some bugs fixed

--- Express with Mongoose/index.js
+++ Express with Mongoose/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/products");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/productsDB", {
@@ -30,7 +33,7 @@ app.get("/products", async (req, res) => {
   res.render("products/list", { products });
 });
 
-app.get("/products/details/:id", async (req, res) => {
+app.get("/products/:id/details", async (req, res) => {
   const { id } = req.params;
   let product = await Product.findById(id);
   res.render("products/details", { product });
@@ -40,10 +43,25 @@ app.get("/products/new", (req, res) => {
   res.render("products/new");
 });
 
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  let product = await Product.findById(id);
+  res.render("products/update", { product });
+});
+
 app.post("/products", async (req, res) => {
   const product = new Product(req.body);
   await product.save();
   res.redirect("/products");
+});
+
+app.put("/products/:id/update", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${id}/details`);
 });
 
 app.listen(3000, () => {
