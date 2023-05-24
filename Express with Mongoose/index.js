4717175cb16c965f71de70d21465fb9fd13fa8a2
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-05-24 15:02:33 +0300

    Bug fixed for adding new routes and adding new product function is added

--- Express with Mongoose/index.js
+++ Express with Mongoose/index.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/productsDB", {
@@ -20,15 +21,29 @@ mongoose
     console.log("Connection error ", e);
   });
 
+app.get("/", (req, res) => {
+  res.redirect("/products");
+});
+
 app.get("/products", async (req, res) => {
   let products = await Product.find({});
   res.render("products/list", { products });
 });
 
-app.get("/products/:id", async (req, res) => {
+app.get("/products/details/:id", async (req, res) => {
   const { id } = req.params;
   let product = await Product.findById(id);
   res.render("products/details", { product });
+});
+
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
+app.post("/products", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.redirect("/products");
 });
 
 app.listen(3000, () => {
