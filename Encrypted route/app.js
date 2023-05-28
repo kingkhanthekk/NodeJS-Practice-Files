79a8eedaf407d636cd486e6eee26afe078c4ed6d
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-05-28 14:24:31 +0300

    Add custom error handlers

--- Encrypted route/app.js
+++ Encrypted route/app.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const AppError = require("./AppError");
 
 const app = express();
 
 const verify = (req, res, next) => {
   const { password } = req.query;
   if (password === "gulumulu") next();
-  res.send("You have to include corret password.");
+  throw new AppError(401, "You require password.");
+  // res.send("You have to include corret password.");
 };
 
 app.get("/secret", verify, (req, res) => {
   res.send("My secret is I feel so lonely all the time.");
+});
+
+app.get("/error", (req, res) => {
+  jhinku.go();
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong." } = err;
+  console.log(err);
+  res.status(status).send(message);
 });
 
 app.listen(3000, () => {
