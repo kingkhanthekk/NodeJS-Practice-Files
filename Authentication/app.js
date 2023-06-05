b2d74cb77d776bd4fc2e135ad27485964c7fd21c
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-06-05 13:57:40 +0300

    Created login get and post routes, added functionality, fixed some bugs.

--- Authentication/app.js
+++ Authentication/app.js
@@ -4,11 +4,13 @@ const User = require("./models/user");
 const path = require("path");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "thisisasecret" }));
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/authDB", {
@@ -30,6 +32,15 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/secret", (req, res) => {
+  if (!req.session.userId) return res.redirect("/login");
+  res.send("Isko ki kisko ki chutki.");
+});
+
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const hash = await bcrypt.hash(password, 12);
@@ -38,7 +49,18 @@ app.post("/register", async (req, res) => {
     password: hash,
   });
   await user.save();
-  res.redirect("/");
+  req.session.userId = user._id;
+  res.redirect("/secret");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  const pass = await bcrypt.compare(password, user.password);
+  if (pass) {
+    req.session.userId = user._id;
+    res.redirect("/secret");
+  } else res.redirect("/login");
 });
 
 app.listen(3000, () => {
