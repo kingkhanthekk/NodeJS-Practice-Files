5b05dddd50271c4b8e4b1fee9cf10f5cc243fde9
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-06-15 18:57:16 +0300

    Added list and read methods, improved some codes.

--- Terminal app/app.js
+++ Terminal app/app.js
@@ -41,8 +41,15 @@ yargs.command({
 yargs.command({
   command: "read",
   describe: "Read a note",
-  handler: () => {
-    console.log("Reading a note...");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    notes.readNote(argv.title);
   },
 });
 
@@ -50,7 +57,7 @@ yargs.command({
   command: "list",
   describe: "List all notes",
   handler: () => {
-    console.log("Listing all notes...");
+    notes.listNotes();
   },
 });
 
--- Terminal app/notes.js
+++ Terminal app/notes.js
@@ -3,10 +3,8 @@ const fs = require("fs");
 const addNote = (title, body) => {
   const notes = loadNotes();
 
-  const duplicate = notes.filter((note) => {
-    return note.title === title;
-  });
-  if (duplicate.length > 0) {
+  const duplicate = notes.find((note) => note.title === title);
+  if (duplicate) {
     console.log("The title is already taken. Choose a new title.");
   } else {
     notes.push({
@@ -33,17 +31,34 @@ const saveNote = (notes) => {
 
 const removeNote = (title) => {
   const notes = loadNotes();
-  const available = notes.filter((note) => {
-    return note.title === title;
-  });
-  if (available.length > 0) {
+  const available = notes.find((note) => note.title === title);
+  if (available) {
     const newNote = notes.filter((note) => {
       return note.title !== title;
     });
     saveNote(newNote);
     console.log("Successfully removed the note:", title);
   } else {
-    console.log("The mentioned title doesn't exixst.");
+    console.log("The mentioned note doesn't exixst.");
+  }
+};
+
+const readNote = (title) => {
+  const notes = loadNotes();
+  const foundNote = notes.find((note) => note.title === title);
+  if (foundNote) {
+    console.log("Title:", foundNote.title);
+    console.log("Body:", foundNote.body);
+  } else {
+    console.log("No note found.");
+  }
+};
+
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log("Your notes:");
+  for (let note of notes) {
+    console.log(note.title);
   }
 };
 
@@ -51,4 +66,6 @@ module.exports = {
   addNote,
   loadNotes,
   removeNote,
+  listNotes,
+  readNote,
 };
